bb5d4736d106ca4cf667faada42827425b9e1c1f
obrienotieno <gebryo@intelligencia.com>
2018-06-25 20:32:31 +0300

    Added python file

--- addtwonumbers.py
+++
@@ -1,7 +0,0 @@
-//Function that adds two numbers
-def add_two_number(num1,num2):
-    return a + b
-	
-if __name__ == '__main__' :
-	num1 = int(input("enter  num1 :"))
-    num2 = int(input("enter  num2 :")) 
